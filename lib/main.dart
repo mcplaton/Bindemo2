91d874431a33a6757f3a62483aa754fab572421a
Mc Platon - افلاطون <98595150+mcplaton@users.noreply.github.com>
2025-07-22 06:46:22 +0300

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:better_player/better_player.dart';
+
+void main() => runApp(PlaTonTV());
+
+class PlaTonTV extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PlaTon TV',
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final String channelUrl = "http://het115a.4rouwanda-shop.store/live/69854211/index.m3u8?t=NuSW89MElHbpt93eabdvug&e=1753095973";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('PlaTon TV')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('تشغيل قناة beIN Sports 2'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => PlayerScreen(url: channelUrl),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class PlayerScreen extends StatefulWidget {
+  final String url;
+  const PlayerScreen({required this.url});
+
+  @override
+  _PlayerScreenState createState() => _PlayerScreenState();
+}
+
+class _PlayerScreenState extends State<PlayerScreen> {
+  late BetterPlayerController _betterPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    BetterPlayerDataSource dataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      widget.url,
+      liveStream: true,
+      useAsmsSubtitles: false,
+    );
+
+    _betterPlayerController = BetterPlayerController(
+      BetterPlayerConfiguration(
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        fit: BoxFit.contain,
+        controlsConfiguration: BetterPlayerControlsConfiguration(
+          enableFullscreen: true,
+        ),
+      ),
+      betterPlayerDataSource: dataSource,
+    );
+  }
+
+  @override
+  void dispose() {
+    _betterPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("beIN Sports 2")),
+      body: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: BetterPlayer(controller: _betterPlayerController),
+      ),
+    );
+  }
+}
